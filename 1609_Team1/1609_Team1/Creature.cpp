9dbaf8a4a7dc7b60504b276d05864bab7331a5c7
LimCHBunkerDefense <gnwmapf@naver.com>
2017-02-06 10:49:35 +0900

    크리쳐 수정중 커먼헤드 크리쳐스티에트 추가

--- 1609_Team1/1609_Team1/Creature.cpp
+++ 1609_Team1/1609_Team1/Creature.cpp
@@ -13,7 +13,12 @@ Creature::~Creature()
 
 void Creature::Update(float deltaTime)
 {
+	switch (m_state)
+	{
+	case PLAYER_ATTACK: AttackState(deltaTime); break;
+	case PLAYER_SHOP: ShopState(); break;
 
+	}
 }
 
 void Creature::Draw(Camera* pCamera)
--- 1609_Team1/1609_Team1/Creature.h
+++ 1609_Team1/1609_Team1/Creature.h
@@ -3,11 +3,18 @@
 class Creature :
 	public Object
 {
+	CREATURE_STATE m_state;
+
 public:
 	Creature();
 	~Creature();
 
 	void Update(float deltaTime);
 	void Draw(Camera* pCamera);
+
+	void RunState(float deltaTime);
+	void AttackState(float deltaTime);
+	void DeadState(float deltaTime);
+
 };
 
