f998da582243b8fafc0dddf8cd38c7e42c382a8b
User <User@KYJ>
2017-02-07 14:58:17 +0900

    임시커밋

--- 1609_Team1/1609_Team1/ShopScene.cpp
+++ 1609_Team1/1609_Team1/ShopScene.cpp
@@ -13,22 +13,24 @@ ShopScene::~ShopScene()
 void ShopScene::OnEnter()
 {
 	NEW_OBJECT(m_pBg, Sprite(RENDER->GetImage(TEXT("shopBG")), 1.0f, 0, 0));
+
+	m_buyButton = Box(Vector(1000, 500), Vector(100, 50));
+	m_weapon1 = Box(Vector(600, 300), Vector(100, 50));
+	m_weapon2 = Box(Vector(600, 400), Vector(100, 50));
+	m_weapon3 = Box(Vector(600, 500), Vector(100, 50));
 }
 
 void ShopScene::OnUpdate(float deltaTime)
 {
-	// ¾À Ã¤ÀÎÁö Ãß°¡ (170207 ±èÀ±Áß)
-	if (INPUT->IsKeyDown(VK_F3))
-	{
-		SCENE->ChangeScene(SCENE_PLAY);
-	}
+	OBJECT->Update(deltaTime);
+
 	ItemListWnd();
 	ItemStatWnd();
 }
 
 void ShopScene::OnExit()
 {
-	
+
 }
 
 void ShopScene::OnDraw()
@@ -40,11 +42,18 @@ void ShopScene::OnDraw()
 	RENDER->DrawRect(Vector(1100, 800), Vector(150, 50), ColorF::Blue);
 	RENDER->DrawT(TEXT("³ª°¡±â F3"), 1050, 785, ColorF::Red, 25);
 
+	if (SCENE->GetColliderOnOff())
+	{
+		RENDER->DrawRect(m_buyButton.LeftTop() + m_buyButton.size * 0.5f, m_buyButton.size, ColorF::Yellow, 3.0f);
+		RENDER->DrawRect(m_weapon1.LeftTop() + m_weapon1.size * 0.5f, m_weapon1.size, ColorF::Yellow, 3.0f);
+		RENDER->DrawRect(m_weapon2.LeftTop() + m_weapon2.size * 0.5f, m_weapon2.size, ColorF::Yellow, 3.0f);
+		RENDER->DrawRect(m_weapon3.LeftTop() + m_weapon3.size * 0.5f, m_weapon3.size, ColorF::Yellow, 3.0f);
+	}
 }
 
 void ShopScene::ItemListWnd()
 {
-	RENDER->DrawRect(Vector(400,450),Vector(400,650),ColorF::Red);
+	RENDER->DrawRect(Vector(400, 450), Vector(400, 650), ColorF::Red);
 	RENDER->DrawT(TEXT("ITEM LIST"), 350, 400, ColorF::Red, 25);
 }
 void ShopScene::ItemStatWnd()
