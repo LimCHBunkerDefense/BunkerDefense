8ca927d65b7ba307ab6e9651923e9dbddec82cb2
SeungMoon <cryhaveit@naver.com>
2017-02-07 15:29:48 +0900

    Player 3D 적용중

--- 1609_Team1/1609_Team1/Player.cpp
+++ 1609_Team1/1609_Team1/Player.cpp
@@ -2,14 +2,23 @@
 
 
 
-Player::Player()
+Player::Player():
+	m_LineCamera(Vector(CHARACTER_X, CHARACTER_Y), MATH->ToDirection(90) * 600),
+	m_LineLeft(Vector(CHARACTER_X, CHARACTER_Y), MATH->ToDirection(CAMERA_LEFT) * 600),
+	m_LineRight(Vector(CHARACTER_X, CHARACTER_Y), MATH->ToDirection(CAMERA_RIGHT) * 600)
 {
 }
 
-Player::Player(OBJ_TAG tag) : Object(tag)
+Player::Player(OBJ_TAG tag) : Object(tag),
+m_LineCamera(Vector(CHARACTER_X, CHARACTER_Y), MATH->ToDirection(90) * 600),
+m_LineLeft(Vector(CHARACTER_X, CHARACTER_Y), MATH->ToDirection(CAMERA_LEFT) * 600),
+m_LineRight(Vector(CHARACTER_X, CHARACTER_Y), MATH->ToDirection(CAMERA_RIGHT) * 600)
 {
 	m_state = PLAYER_ATTACK;
 	m_dir=Vector::Right();
+	m_height = GROUND_HEIGHT;
+	PrevMousePos = INPUT->GetMousePos();
+	Sight = SIGHT;
 }
 
 Player::~Player()
@@ -49,6 +58,30 @@ void Player::AttackState(float deltaTime)
 	{
 		SCENE->SetColliderOnOff();
 	}
+
+	//마우스 누르면 각도 돌아가기
+	float fTurnSpeed = 0;
+	if (INPUT->IsKeyPress(VK_LEFT)) {
+		//m_angle -= 10 * deltaTime;
+		fTurnSpeed = -ROTATE_SPEED;
+	}
+	if (INPUT->IsKeyPress(VK_RIGHT)) {
+		//m_angle += 10 * deltaTime;
+		fTurnSpeed = ROTATE_SPEED;
+	}
+	if (INPUT->IsKeyPress(VK_UP)) {
+		//m_angle -= 10 * deltaTime;
+		m_height += ROTATE_SPEED * 5;
+	}
+	if (INPUT->IsKeyPress(VK_DOWN)) {
+		//m_angle += 10 * deltaTime;
+		m_height -= ROTATE_SPEED * 5;
+	}
+	/*FOR_LIST(Line*, m_listLine) {
+		float prev_angle = MATH->Angle(m_dir, (*it)->EndPoint() - (*it)->StartPoint());
+		Vector result_dir = MATH->ToDirection(prev_angle + fTurnSpeed);
+		(*it)->SetEndPoint(m_player.center + result_dir * 600);
+	}*/
 }
 
 void Player::ShopState()
