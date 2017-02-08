3867eee23434aed6099e1c0086b67321fc2d2876
SeungMoon <cryhaveit@naver.com>
2017-02-08 21:09:20 +0900

    Bullet 추가

---
+++ 1609_Team1/1609_Team1/Bullet.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "ObjectManager.h"
+#include "CreatureDB.h"
+
+class Bullet :
+	public Object
+{
+	
+	Sprite* m_pSprite;
+	float m_moveSpeed;
+
+	float m_scale;
+	Vector m_moveDirection;
+	DIRECTION_TAG m_dir;
+
+public:
+	Bullet();
+	Bullet(OBJ_TAG tag);
+	~Bullet();
+
+	void Update(float deltaTime);
+	void Draw(Camera* pCamera);
+
+	void SetMoveDirection(Vector vector) { m_moveDirection = vector; }
+	Vector GetMoveDirection() { return m_moveDirection; }
+
+};
+
