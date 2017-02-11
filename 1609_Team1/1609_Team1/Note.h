44d8a30697ce870b3aa20da862ee6974130da3eb
LimCHBunkerDefense <gnwmapf@naver.com>
2017-02-11 10:05:45 +0900

    메모장 수정
    170211 10:05

--- 1609_Team1/1609_Team1/Note.h
+++ 1609_Team1/1609_Team1/Note.h
@@ -1,21 +1,66 @@
 /*
  [ 메 모 장 ]
- 
- <완성>
- - 게임메니저, 인풋메니저, 씬메니저, 사운드메니저, 윈도우메니저, 메인, 싱글턴, 커먼 완성
 
- <할일>
- - 플레이씬 생성
- - 윈도우창 뜨는지 확인
- - 렌더메니저 수정된 것으로 교체
- - ObjMgr 전체적 수정할 차례
- - 매스에 최근 수업내용 추가
- - Obj 생성
- - Obj Data 및 Obj DB 생성
- - Obj는 플레이어, 크리쳐, 총알, 벙커
- - 아이템 구성 고안
+ Database
+	CreatrueDB
+		- 크리쳐 데이터 생성 필요
+	ItemDB
+		- 게임 구동 후 테스트하여 ItemDB 수정 필요
 
- - UI, 음향
+Manager
+	GameMgr : 완료
+	InputMgr : 완료
+	ObjMgr
+		Item 관련 추가 필요 (ex DestroyItem)
+	RenderMgr
+		Camera
+			Draw3D & 좌표 수정함수 검토 필요
+	SceneMgr : 완료
+	SoundMgr : 완료
+	WndMgr : 완료
+
+Object
+	Bullet
+		Draw 부분, Camera에 따라 수정 필요 (Camera 완성 후)
+		Explsion Ani 테스트 미완료
+		Craeture에 AddLife와 같은 함수 추가하여 Life깎이는것 확인 필요
+		Sprite 작업 필요
+	Bunker
+		전체적으로 안되어있음
+		Creature가 벙커에 데미지 입힐 때, 관련 시각적 효과 추가 필요
+	Button
+		진도상황 모름 ㅋㅋㅋ 만드는 자가 체크하길..
+	Creature
+		AddLife 등 함수 추가
+		Sprite 작업 필요
+	Item
+		Sprite 작업 필요
+		Player가 사용하는 것에 대한 테스트 미완료
+	Player
+		각 무기에 따른 이미지 출력여부 테스트 미완료
+		마우스 감도에 따른 화면 움직이는 것으로 수정 필요
+		esc등 키를 이용하여 마우스 감도 수정하도록 변경
+		usingItem 사용여부 테스트 미완료
+
+Scene
+	ShopScene
+		Sprite 작업 필요
+		글자체 변경 필요
+		수량 변경부분 수정
+	PlayScene
+		배경 움직이는 것 수정 필요
+		UI 작업 전반적으로 필요
+		Minimap 위치 및 크기 수정 필요
+		Bullet 나가는 것 화면에 출력하는 부분 수정 필요
+	StartScene
+		구상 및 구현 필요 (작업 막바지때)
+	LoadingScene
+		구상 및 구현 필요 (작업 막바지때)
+	EndingScene
+		구상 및 구현 필요 (작업 막바지때)
+
+소리작업 전체 필요
+
  
  
  
