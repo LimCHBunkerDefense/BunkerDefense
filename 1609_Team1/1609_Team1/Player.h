fcf8983fff413f07c72dd6b03863aa69747442e0
LSM <cryhaveit@naver.com>
2017-02-14 12:54:01 +0900

    Player에서 아이템 스테이트 저장 및 리턴

--- 1609_Team1/1609_Team1/Player.h
+++ 1609_Team1/1609_Team1/Player.h
@@ -14,9 +14,10 @@ class Player :
 	map<int, Object*> m_itemBag;			// 아이템 가방
 
 	PLAYER_STATE m_state;				// 플레이어 상태
+	ITEM_TAG item_state;				//아이템 가진 상태
 										   
 	Vector m_prevMousePos;				// 이전 마우스 좌표값
-	float m_sight;						// 시야 길이
+	//float m_sight;						// 시야 길이
 
 
 
@@ -36,5 +37,7 @@ public:
 
 	// 번호 입력시 아이템을 장착하는 함수
 	void SetItem();
+
+	ITEM_TAG GetItemState() { return item_state; }
 };
 
